e6fc76b49583053fcb285dfdb98afae83443deff
Praneeth Reddy Nimma <nimmapraneethreddy@gmail.com>
2025-02-12 14:17:23 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function predictAdmission() {
         research: parseInt(document.getElementById("research").value)
     };
 
-    fetch("http://127.0.0.1:5000/predict", {
+    fetch("https://university-admission-frontend.onrender.com", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
